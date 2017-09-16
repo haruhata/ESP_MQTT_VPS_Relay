b4a65d9a3aa08d0094bee0c53080219763866a2a
neillp <neillp@users.noreply.github.com>
2017-09-16 15:49:56 +0200

    Add files via upload

--- Mosquitto_2.ino
+++ Mosquitto_2.ino
@@ -2,24 +2,16 @@
 /*
 
  It connects to an MQTT server then:
-  - on 0 switches off relay
-  - on 1 switches on relay
-
-  - sends 0 on off relay
-  - sends 1 on on relay
+  - '0' switches off relay
+  - '1' switches on relay
 
  It will reconnect to the server if the connection is lost using a blocking
  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
  achieve the same result without blocking the main loop.
-
- 
-
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-
-
 
 const char* ssid = "pieterse";
 const char* password = "yours246";
@@ -37,13 +29,10 @@ const char* RelayTopic = "/switch/relayX";
 int relay_pin = 5;
 bool relayState = LOW;
 
-
-
-
 void setup_wifi() {
-
   delay(10);
-  // We start by connecting to a WiFi network
+  
+                                    // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -59,13 +48,6 @@ void setup_wifi() {
     }
     Serial.print(".");
   }
-  digitalWrite(13, LOW);
-  delay(500);
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
-  digitalWrite(13, HIGH);
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -81,42 +63,36 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  // Switch on the LED if an 1 was received as first character
+                                               // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '0') {
-    digitalWrite(relay_pin, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(relay_pin, LOW);              // Turn the LED on (Note that LOW is the voltage level
     Serial.println("relay_pin -> LOW");
     relayState = LOW;
 
   } else if ((char)payload[0] == '1') {
-    digitalWrite(relay_pin, HIGH);  // Turn the LED off by making the voltage HIGH
+    digitalWrite(relay_pin, HIGH);             // Turn the LED off by making the voltage HIGH
     Serial.println("relay_pin -> HIGH");
     relayState = HIGH;
   
-/*  } else if ((char)payload[0] == '2') {
-     relayState = !relayState;
-     digitalWrite(relay_pin, relayState);  // Turn the LED off by making the voltage HIGH
-     Serial.print("relay_pin -> switched to ");
-     Serial.println(relayState);
- */  
   }
 }
 
 void reconnect() {
-  // Loop until we're reconnected
+                                              // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
+                                              // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish(outTopic, "Sonoff1 booted");
-      // ... and resubscribe
+                                              // Once connected, publish an announcement...
+      client.publish(outTopic, "Sonoff1 booted");   
+                                              // ... and resubscribe
       client.subscribe(RelayTopic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
+                                             // Wait 5 seconds before retrying
       for(int i = 0; i<5000; i++){
         
         delay(1);
@@ -128,22 +104,15 @@ void reconnect() {
 
 
 void setup() {
-               // Begin eeprom to store on/off state
-  pinMode(relay_pin, OUTPUT);     // Initialize the relay pin as an output
+              
+  pinMode(relay_pin, OUTPUT);               // Initialize the relay pin as an output
 
   pinMode(5, OUTPUT);
   
   digitalWrite(relay_pin,relayState);
- 
-
- 
-  digitalWrite(13, LOW);          // Blink to indicate setup
-  delay(500);
-  digitalWrite(13, HIGH);
-  delay(500);
- 
+  
   Serial.begin(115200);
-  setup_wifi();                   // Connect to wifi
+  setup_wifi();                             // Connect to wifi
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
